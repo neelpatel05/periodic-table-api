5904dbb7a91f99a0850e50c2bb2b344779cbf431
Neel Patel <neel.patel6573@gmail.com>
2019-03-19 12:22:28 +0530

    removes the file

--- home/neelpatel05/csvtodict.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import json
-
-csv_data = pd.read_csv('data.csv')
-data = csv_data.to_json(orient='records')
-
-with open('data.json','w') as outfile:
-    json.dump(data,outfile)
